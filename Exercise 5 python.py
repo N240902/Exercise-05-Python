ee2c4bfaa979c60471d994b375162f357171cbc9
N240902 <nandiphazigwebile@gmail.com>
2024-11-08 11:12:03 +0200

    added exercise 5

---
+++ Exercise 5 python.py
@@ -0,0 +1,80 @@
+# Question 1: Creating and Modifying Lists
+
+# # TODO: Create a list of fruits
+# fruits=["orange", "banana","apple","pomelo","avocado","cherry"]
+# print(fruits)
+
+# # TODO: Add a fruit to the end of the list
+# fruits.append("strawberry")
+# print(fruits)
+
+# # TODO: Insert a fruit at the beginning of the list
+# fruits=["grapes", "orange","banana","pomelo","apple","avocado","cherry","strawberry"]
+
+# # TODO: Remove a fruit from the list
+# fruits.remove("cherry")
+
+# # TODO: Print the modified list
+# print(fruits)
+#-------------------------------------------------------------------------
+# Question 2: List Operations
+
+# TODO: Create a list of numbers from 1 to 5
+# list=[1,2,3,4,5]
+# print(list)
+
+# TODO: Create a new list with each number squared
+# squared_number=((2*2), (4*4), (5*5), (3*3),(1*1))
+# print(squared_number)
+# TODO: Find the sum and average of the original numbers
+# sum=(1+2+3+4+5)
+# print(sum)
+# average=sum/5
+
+# # TODO: Print the results
+# print(average)
+#-------------------------------------------------------------------------
+# Question 3: Creating and Modifying Dictionaries
+
+# TODO: Create a dictionary of countries and their capitals
+# countries= {
+#     "Japan" : "Tokyo",
+#     "Canada" :"Ottawa",
+#     "France" : "Paris",
+#     "Italy": "Rome"
+#     }
+# print(countries)
+
+# # TODO: Add a new country-capital pair
+# countries["Spain"]="Madrid"
+# print(countries)
+# # Update the capital of an existing country
+# countries["Italy"]="Rome"
+# print(countries)
+# # TODO: Remove a country-capital pair
+# del countries["Italy"]
+
+# # TODO: Print the modified dictionary
+# print(countries)
+#-------------------------------------------------------------------------
+# Question 4: Dictionary Operations
+
+# TODO: Create a dictionary of fruit colors
+colors={
+    "Banana" :"Yellow",
+    "Apple" :"Green",
+    "Orange" :"Orange",
+}
+print(colors)
+# TODO: Print all the fruits (keys)
+for key in colors:
+    print(key)
+
+# TODO: Print all the colors (values)
+print(colors.values())
+
+# TODO: Print each fruit and its color\
+for keys_and_values in colors:
+    if colors== ("Banana"):
+        a+=1
+        print(colors)
